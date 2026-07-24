28e7c5adc26b6b69e5839f376077c00efffd02b8
hassanmohd60004-source <hassanmohd60004-source@users.noreply.github.com>
2026-07-24 04:59:29 -0700

    Remove envelope landing animation and display main invitation directly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { weddingConfig } from "@/config/wedding";
-import BookCover from "@/components/BookCover";
 import HeroInvitation from "@/components/HeroInvitation";
 import InvitationMessage from "@/components/InvitationMessage";
 import EventDetails from "@/components/EventDetails";
@@ -12,53 +11,43 @@ import RSVP from "@/components/RSVP";
 import BottomNav from "@/components/BottomNav";
 
 export default function Home() {
-  const [isOpenComplete, setIsOpenComplete] = useState(false);
-
   return (
-    <main className="min-h-screen relative bg-background">
-      {/* ═══ BOOK COVER LANDING SCREEN ═══ */}
-      {!isOpenComplete && (
-        <BookCover onComplete={() => setIsOpenComplete(true)} />
-      )}
+    <main className="min-h-screen relative bg-background flex flex-col pb-20 md:pb-0">
+      {/* Hero Section */}
+      <HeroInvitation
+        brideName={weddingConfig.brideName}
+        groomName={weddingConfig.groomName}
+        countdownDate={weddingConfig.countdownDate}
+        hijriDate={weddingConfig.hijriDate}
+        gregorianDate={weddingConfig.gregorianDate}
+      />
 
-      {/* ═══ MAIN WEBSITE CONTENT ═══ */}
-      <div className="flex flex-col min-h-screen pb-20 md:pb-0">
-        {/* Hero Section */}
-        <HeroInvitation
-          brideName={weddingConfig.brideName}
-          groomName={weddingConfig.groomName}
-          countdownDate={weddingConfig.countdownDate}
-          hijriDate={weddingConfig.hijriDate}
-          gregorianDate={weddingConfig.gregorianDate}
-        />
+      {/* Invitation Message */}
+      <InvitationMessage text={weddingConfig.invitationText} />
 
-        {/* Invitation Message */}
-        <InvitationMessage text={weddingConfig.invitationText} />
+      {/* Google Maps Location */}
+      <GoogleMaps
+        embedUrl={weddingConfig.googleMapsEmbedUrl}
+        mapsLink={weddingConfig.googleMapsLink}
+        hallName={weddingConfig.hallName}
+      />
 
-        {/* Google Maps Location */}
-        <GoogleMaps
-          embedUrl={weddingConfig.googleMapsEmbedUrl}
-          mapsLink={weddingConfig.googleMapsLink}
-          hallName={weddingConfig.hallName}
-        />
+      {/* Event Details */}
+      <EventDetails
+        hallName={weddingConfig.hallName}
+        address={weddingConfig.address}
+        gregorianDate={weddingConfig.gregorianDate}
+        hijriDate={weddingConfig.hijriDate}
+      />
 
-        {/* Event Details */}
-        <EventDetails
-          hallName={weddingConfig.hallName}
-          address={weddingConfig.address}
-          gregorianDate={weddingConfig.gregorianDate}
-          hijriDate={weddingConfig.hijriDate}
-        />
+      {/* Event Timeline */}
+      <Timeline events={weddingConfig.timeline} />
 
-        {/* Event Timeline */}
-        <Timeline events={weddingConfig.timeline} />
+      {/* RSVP Form */}
+      <RSVP />
 
-        {/* RSVP Form */}
-        <RSVP />
-
-        {/* Mobile Bottom Floating Nav Bar */}
-        <BottomNav />
-      </div>
+      {/* Mobile Bottom Floating Nav Bar */}
+      <BottomNav />
     </main>
   );
 }
